6285b36a93440504c656d12cd619dcb2ed5ea130
Jonathan Dushoff <dushoff@mcmaster.ca>
2016-01-10 23:34:35 -0500

    Autocommit (Bio3SS.github.io) On branch master Your branch is up-to-date with 'origin/master'.
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	new file:   _drafts/tut1.md
    	new file:   _posts/2016-01-10-Tutorials.md
    	modified:   stuff.mk

--- stuff.mk
+++ stuff.mk
@@ -1,9 +1,3 @@
-%.makestuff:
-	-cd $(dir $(ms)) && mv -f $(notdir $(ms)) .$(notdir $(ms))
-	cd $(dir $(ms)) && git clone $(msrepo)/$(notdir $(ms)).git
-	-cd $(dir $(ms)) && rm -rf .$(notdir $(ms))
-	touch $@
-
 msrepo = https://github.com/dushoff
 gitroot = ../
 export ms = $(gitroot)/makestuff
